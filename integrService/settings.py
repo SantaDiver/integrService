df59f8eab8e72abb3231ecd1b7dcd271946517a0
Nikolay Sav <nik7101@yandex.ru>
2018-02-06 14:46:09 +0300

    Added allowed hosts

--- integrService/settings.py
+++ integrService/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '1y7#)4^x3q%ee1^j#q3fd-(en*yhup_=ig-hkz2jm0=(-f=7o0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'snmplatform.ru', '188.225.26.210']
 
 # Application definition
 
