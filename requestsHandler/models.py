f5f3e5fcbbd5f2395ba2a0e3a8c8b073eb0ef4a9
NikolaySav <nik7101@yandex.ru>
2018-03-09 14:22:38 +0300

    SSL provided

--- requestsHandler/models.py
+++ requestsHandler/models.py
@@ -13,10 +13,10 @@ def _createHash():
     hash = hashlib.sha1()
     hash.update(str(time.time()).encode('utf-8'))
     return  hash.hexdigest()
-    
+
 def get_default_user():
     return User.objects.get(id=1)
-    
+
 def get_default_config():
     return {
         'cache_ttl' : 300,
@@ -36,18 +36,18 @@ def get_default_rights():
 
 # Create your models here.
 class UserConfig(models.Model):
-    public_hash = models.CharField(max_length=40, default=_createHash(), 
+    public_hash = models.CharField(max_length=40, default=_createHash,
         unique=True, db_index=True)
-    private_hash = models.CharField(max_length=40, default=_createHash(), 
+    private_hash = models.CharField(max_length=40, default=_createHash, 
         unique=True, db_index=True)
-    
+
     user = models.OneToOneField(User, default=get_default_user)
-    
+
     config = JSONField(default=get_default_config(), blank=True)
     account_rights = JSONField(default=get_default_rights(), blank=True)
     cache = JSONField(default={}, blank=True)
     fields_cache = JSONField(default={}, blank=True)
     last_user_cache = JSONField(default={}, blank=True)
-    
-    
+
+
     history = HistoricalRecords()
